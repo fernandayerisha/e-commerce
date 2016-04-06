91274362ccc1dc46159a930769bb351a49b7588c
yaik <faiqyaik@gmail.com>
2016-04-06 09:51:47 +0700

    tabel seller

---
+++ 2016_04_05_093727_create_table_toko.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableToko extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->increments('id_seller');
+            $table->string('nama',50);
+            $table->string('cont',20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sellers');
+    }
+}
