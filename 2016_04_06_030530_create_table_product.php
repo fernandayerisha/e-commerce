f71c2a031fa9560eff5775706f5141b51f774fbe
Yunizar Isnaini <Yunizar Isnaini>
2016-04-06 10:37:06 +0700

    product

---
+++ 2016_04_06_030530_create_table_product.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id_product');
+            $table->string('nama_product');
+            $table->string('kategori_product');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product');
+    }
+}
